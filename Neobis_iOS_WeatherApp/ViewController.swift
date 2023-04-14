1b666afe02a678cb2f633afda4d183ffbf32b00b
Togosling <114942562+Togosling@users.noreply.github.com>
2023-04-14 20:36:58 +0800

    first commit

---
+++ Neobis_iOS_WeatherApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Neobis_iOS_WeatherApp
+//
+//  Created by Тагай Абдылдаев on 14/4/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
